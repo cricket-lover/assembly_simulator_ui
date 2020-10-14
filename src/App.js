8c2e70470037558187491aad80cdc3cd61843435
phaneendra <phaneendraandukuri@gmail.com>
2020-10-14 13:04:47 +0530

    added a simple textarea to take and run the program

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Machine from '@craftybones/assembly_simulator';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const machine = new Machine();
+  const [value, setValue] = useState('');
+  const [output, setOutput] = useState('');
+
+  const handleClick = function (event) {
+    console.log(value);
+    machine.load(value);
+    machine.execute();
+    setOutput(machine.getPrn().join('\n'));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <textarea type="text" onChange={(e) => setValue(e.target.value)} />
+      <button onClick={handleClick}>Run</button>
+      <div>{output}</div>
     </div>
   );
 }
